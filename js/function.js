a13c24e542c720bc4dccdbd860873fa018e3b890
sannavirkki <sannahannelevirkki@outlook.com>
2024-09-30 19:44:01 +0200

    viimonen yritys

--- js/function.js
+++ js/function.js
@@ -2,6 +2,6 @@ function rollDice() {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
 
     const diceImage = document.querySelector("#dice img");
-    diceImage.src = `../noppa/kuvat/${randomNumber}noppa.png`;
+    diceImage.src = `./kuvat/${randomNumber}noppa.png`;
 }
 document.querySelector("#dice img").addEventListener("click", rollDice);
